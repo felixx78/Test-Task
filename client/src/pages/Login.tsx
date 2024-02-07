8c367c49af37add48fc2b93e11310b7151543ce5
v1st78 <petrenkov605@gmail.com>
2024-02-07 18:59:10 +0200

    fix handleLogin Login Page

--- client/src/pages/Login.tsx
+++ client/src/pages/Login.tsx
@@ -51,7 +51,8 @@ function Login() {
           });
         }
       } else {
-        dispatch(userActions.auth(data));
+        localStorage.setItem("token", data.acessToken);
+        dispatch(userActions.auth(data.payload));
         setTimeout(() => navigate("/"), 0);
       }
     });
