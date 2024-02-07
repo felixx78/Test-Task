56034c79dd4f9ec55b54ae04c7724a8b820db1bc
v1st78 <petrenkov605@gmail.com>
2024-02-07 21:25:29 +0200

    add Signup Page

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -3,11 +3,13 @@ import Home from "./pages/Home";
 import RootLayout from "./layouts/RootLayout";
 import RequireAuth from "./components/RequireAuth";
 import Login from "./pages/Login";
+import Signup from "./pages/Signup";
 
 function App() {
   return (
     <Routes>
       <Route path="/login" element={<Login />} />
+      <Route path="/signup" element={<Signup />} />
 
       <Route path="/" element={<RequireAuth children={<RootLayout />} />}>
         <Route index element={<Home />} />
---
+++ client/src/pages/Signup.tsx
@@ -0,0 +1,114 @@
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+import { ZodType, z } from "zod";
+import AuthInput from "../components/AuthInput";
+import { Link, useNavigate } from "react-router-dom";
+import { useDispatch } from "react-redux";
+import { userActions } from "../redux/userReducer";
+
+type FormData = {
+  username: string;
+  password: string;
+  confirmPassword: string;
+};
+
+function Signup() {
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+
+  const schema: ZodType<FormData> = z
+    .object({
+      username: z
+        .string()
+        .min(2)
+        .max(16)
+        .refine((value) => /^[^0-9]/.test(value), {
+          message: "Username cannot start with a number",
+        }),
+      password: z.string().min(4).max(32),
+      confirmPassword: z.string().min(4).max(32),
+    })
+    .refine((data) => data.password === data.confirmPassword, {
+      message: "Passwords do not match",
+      path: ["confirmPassword"],
+    });
+
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<FormData>({
+    resolver: zodResolver(schema),
+  });
+
+  const handleSignup = async (formData: FormData) => {
+    const response = await fetch("http://localhost:3000/api/auth/signup", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(formData),
+    });
+
+    const data = await response.json();
+
+    localStorage.setItem("token", data.acessToken);
+    dispatch(userActions.auth(data.payload));
+    navigate("/");
+  };
+
+  return (
+    <div className="flex h-screen min-h-[400px] items-center bg-background px-4 pt-24 text-copy">
+      <form
+        onSubmit={handleSubmit(handleSignup)}
+        className="mx-auto mt-[-150px] min-w-[290px] max-w-[400px] rounded-md bg-foreground px-4 py-6"
+      >
+        <h1 className="mb-2 text-center text-xl">Sign up</h1>
+
+        <AuthInput
+          label="Username"
+          name="username"
+          register={register}
+          isError={!!errors.username}
+          errorMessage={errors.username?.message}
+        />
+
+        <div className="mb-2"></div>
+
+        <AuthInput
+          label="Password"
+          name="password"
+          type="password"
+          register={register}
+          isError={!!errors.password}
+          errorMessage={errors.password?.message}
+        />
+
+        <div className="mb-2"></div>
+
+        <AuthInput
+          label="Confirm Password"
+          name="confirmPassword"
+          type="password"
+          register={register}
+          isError={!!errors.confirmPassword}
+          errorMessage={errors.confirmPassword?.message}
+        />
+
+        <div className="mb-4"></div>
+
+        <button className="mb-6 w-full bg-border py-1" type="submit">
+          Submit
+        </button>
+
+        <div>
+          Have already an account?{" "}
+          <Link className="text-primary" to="/login">
+            Login
+          </Link>
+        </div>
+      </form>
+    </div>
+  );
+}
+export default Signup;
