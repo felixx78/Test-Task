fd1cb33782e39a1abcb5842e3e975d211d99ee6d
v1st78 <petrenkov605@gmail.com>
2024-02-07 15:59:27 +0200

    add RequireAuth to Routes

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,11 +1,12 @@
 import { Route, Routes } from "react-router-dom";
 import Home from "./pages/Home";
 import RootLayout from "./layouts/RootLayout";
+import RequireAuth from "./components/RequireAuth";
 
 function App() {
   return (
     <Routes>
-      <Route path="/" element={<RootLayout />}>
+      <Route path="/" element={<RequireAuth children={<RootLayout />} />}>
         <Route index element={<Home />} />
       </Route>
     </Routes>
