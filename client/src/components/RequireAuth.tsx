f35b4cb8a1b9f2b21d4ff4671cd4bc7bdc221042
v1st78 <petrenkov605@gmail.com>
2024-02-07 21:11:01 +0200

    refactor RequireAuth

--- client/src/components/RequireAuth.tsx
+++ client/src/components/RequireAuth.tsx
@@ -1,9 +1,7 @@
-import { useSelector } from "react-redux";
-import { RootState } from "../redux/store";
 import { Navigate } from "react-router-dom";
 
 function RequireAuth({ children }: { children: React.ReactNode }) {
-  const user = useSelector((state: RootState) => state.user);
+  const user = localStorage.getItem("user");
 
   return user ? children : <Navigate replace to="/login" />;
 }
