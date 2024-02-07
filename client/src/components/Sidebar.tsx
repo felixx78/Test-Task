47da4dda3f0872c96d8d7aec3396e53e68ef4e11
v1st78 <petrenkov605@gmail.com>
2024-02-07 02:35:12 +0200

    remove isLogged in Sidebar

--- client/src/components/Sidebar.tsx
+++ client/src/components/Sidebar.tsx
@@ -11,36 +11,27 @@ function Sidebar() {
     {
       name: "Add",
       href: "/add",
-      authRequire: true,
     },
   ];
 
-  const isLogged = true;
-
   return (
-    <aside className="text-background bg-primary flex w-full items-center justify-between px-4 py-2 sm:h-screen sm:w-[200px] sm:flex-col sm:items-stretch sm:px-0 sm:pt-4">
+    <aside className="flex w-full items-center justify-between bg-primary px-4 py-2 text-background sm:h-screen sm:w-[200px] sm:flex-col sm:items-stretch sm:px-0 sm:pt-4">
       <div className="text-center text-xl font-bold sm:mb-12">Domains</div>
-      <div className="divide-primary-light border-primary-light flex gap-4 sm:flex-col sm:gap-0 sm:divide-y-2 sm:border-y-2">
-        {items.map((i) => {
-          if (i.authRequire && !isLogged) return null;
-
-          return (
-            <Link
-              className={`${location.pathname === i.href ? "bg-primary-light" : ""} w-full p-2 text-center sm:block`}
-              to={i.href}
-              key={i.name}
-            >
-              {i.name}
-            </Link>
-          );
-        })}
+      <div className="flex gap-4 divide-primary-light border-primary-light sm:flex-col sm:gap-0 sm:divide-y-2 sm:border-y-2">
+        {items.map((i) => (
+          <Link
+            className={`${location.pathname === i.href ? "bg-primary-light" : ""} w-full p-2 text-center sm:block`}
+            to={i.href}
+            key={i.name}
+          >
+            {i.name}
+          </Link>
+        ))}
       </div>
 
       <div className="hidden flex-1 sm:block"></div>
 
-      {isLogged && (
-        <button className="sm:bg-primary-dark sm:py-2">Logout</button>
-      )}
+      <button className="sm:bg-primary-dark sm:py-2">Logout</button>
     </aside>
   );
 }
