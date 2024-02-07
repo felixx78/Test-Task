f8570626eec933a8707c25a8b9b716e3d9214620
v1st78 <petrenkov605@gmail.com>
2024-02-07 18:38:23 +0200

    add AuthInput

---
+++ client/src/components/AuthInput.tsx
@@ -0,0 +1,48 @@
+import { EyeIcon, EyeSlashIcon } from "@heroicons/react/24/outline";
+import { useState } from "react";
+
+function AuthInput({
+  label,
+  type = "text",
+  name,
+  isError = false,
+  errorMessage,
+  register,
+}: {
+  label: string;
+  type?: string;
+  name?: string;
+  isError?: boolean;
+  errorMessage?: string;
+  register?: any;
+}) {
+  const [isShow, setIsShow] = useState(false);
+
+  return (
+    <div>
+      <label className="mb-1 block">{label}</label>
+      <div className="relative">
+        <input
+          {...register(name)}
+          className={`${isError ? "border-error" : "border-border"} mb-1 block w-full border-2 px-2 py-1 outline-none`}
+          type={type !== "password" || isShow ? "text" : "password"}
+        />
+        {type === "password" && (
+          <button
+            onClick={() => setIsShow(!isShow)}
+            type="button"
+            className="absolute right-2 top-1/2 -translate-y-1/2 transform"
+          >
+            {isShow ? (
+              <EyeSlashIcon className="h-6 w-6" />
+            ) : (
+              <EyeIcon className="h-6 w-6" />
+            )}
+          </button>
+        )}
+      </div>
+      <p className="text-sm text-error">{isError && errorMessage}</p>
+    </div>
+  );
+}
+export default AuthInput;
