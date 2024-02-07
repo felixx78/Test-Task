e2b0afe7893cfc7ee792be0a67ff1b2c51af0ff8
v1st78 <petrenkov605@gmail.com>
2024-02-07 16:18:37 +0200

    fix RequireAuth

--- client/src/components/RequireAuth.tsx
+++ client/src/components/RequireAuth.tsx
@@ -5,6 +5,6 @@ import { Navigate } from "react-router-dom";
 function RequireAuth({ children }: { children: React.ReactNode }) {
   const user = useSelector((state: RootState) => state.user);
 
-  return user.username ? children : <Navigate to="/login" />;
+  return user ? children : <Navigate to="/login" />;
 }
 export default RequireAuth;
