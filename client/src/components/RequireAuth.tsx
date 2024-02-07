eb404f4a1a4a3151d94b102b93e0f7df828d783d
v1st78 <petrenkov605@gmail.com>
2024-02-07 20:48:49 +0200

    add replace Navigate RequireAuth

--- client/src/components/RequireAuth.tsx
+++ client/src/components/RequireAuth.tsx
@@ -5,6 +5,6 @@ import { Navigate } from "react-router-dom";
 function RequireAuth({ children }: { children: React.ReactNode }) {
   const user = useSelector((state: RootState) => state.user);
 
-  return user ? children : <Navigate to="/login" />;
+  return user ? children : <Navigate replace to="/login" />;
 }
 export default RequireAuth;
