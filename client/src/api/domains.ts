b126c2c808c9a129041d5546508b2747cdbc0133
v1st78 <petrenkov605@gmail.com>
2024-02-09 02:55:08 +0200

    add fetchAllDomains

---
+++ client/src/api/domains.ts
@@ -0,0 +1,7 @@
+import { Domain } from "../lib/definition";
+import { privateFetch } from "./privateFetch";
+
+export async function fetchAllDomain() {
+  const data = await privateFetch("http://localhost:3000/api/domain/all");
+  return data as Array<Domain & { _id: string; userId: string }>;
+}
