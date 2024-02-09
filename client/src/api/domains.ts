0463df2e6d038819c54409b578ef9c56df814d36
v1st78 <petrenkov605@gmail.com>
2024-02-09 08:57:01 +0200

    remade fetchAllDomain

--- client/src/api/domains.ts
+++ client/src/api/domains.ts
@@ -1,7 +1,7 @@
 import { Domain } from "../lib/definition";
 import { privateFetch } from "./privateFetch";
 
-export async function fetchAllDomain() {
-  const data = await privateFetch("http://localhost:3000/api/domain/all");
-  return data as Array<Domain>;
+export async function fetchAllDomain(page = 1) {
+  const data = await privateFetch(`http://localhost:3000/api/domain/${page}`);
+  return data as { maxPages: number; result: Array<Domain> };
 }
